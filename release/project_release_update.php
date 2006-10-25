bb9f70bdd44d62d258a6acd1c6871a7561dae3e9
Derek Wright <git@dwwright.net>
2006-10-25 07:04:42 +0000

    #83339: make releases real nodes (see also #58066 and #75053) release_as_node.patch_18.txt by dww (comment 33): better populating of the version_extra field (no need to store the initial '-' in there like we used to).

--- release/project_release_update.php
+++ release/project_release_update.php
@@ -164,7 +164,7 @@ function convert_release($old_release) {
       $node->tag = 'TRUNK';
     }
     else {
-      preg_match('/(\d+)\.(\d+)\.(\d+)(.+)?/', $old_release->version, $matches);
+      preg_match('/(\d+)\.(\d+)\.(\d+)(-.+)?/', $old_release->version, $matches);
       $node->version_major = $matches[1];
       $node->version_minor = $matches[2];
       $node->version_patch = $matches[3];
@@ -177,7 +177,7 @@ function convert_release($old_release) {
     // The "cvs" version is a nightly tarball from the trunk
     $node->version_major = 0;
     $node->version_patch = 0;
-    $node->version_extra = '-dev';
+    $node->version_extra = 'dev';
     $node->tag = 'TRUNK';
     $node->rebuild = 1;
   }
