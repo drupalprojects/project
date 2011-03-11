00cdb924f81efed3619fd721b637f867343aa7d1
Chad Phillips <chad@apartmentlines.com>
2011-03-11 02:31:52 -0600

    remove unnecessary order by clause. add trackObjectIDs method to new releases plugin.

--- release/metrics/ProjectReleaseMetricNewReleases.class.php
+++ release/metrics/ProjectReleaseMetricNewReleases.class.php
@@ -19,7 +19,6 @@ class ProjectReleaseMetricNewReleases extends SamplerMetric {
 
     $sample = $this->currentSample;
     $options = $sample->options;
-
     $args = array($sample->sample_startstamp, $sample->sample_endstamp);
 
     // Restrict to only the passed project nids.
@@ -35,10 +34,19 @@ class ProjectReleaseMetricNewReleases extends SamplerMetric {
     // from {project_release_nodes} to get releases per project, LEFT JOIN
     // with {node} each release node to filter by release node creation date,
     // and then GROUP by the project nid so we can get release counts.
-    $nodes = db_query("SELECT np.nid, COUNT(nr.nid) AS count FROM {node} np LEFT JOIN {project_release_nodes} prn ON np.nid = prn.pid LEFT JOIN {node} nr ON prn.nid = nr.nid AND nr.created > %d AND nr.created < %d$where GROUP BY np.nid ORDER BY NULL", $args);
+    $nodes = db_query("SELECT np.nid, COUNT(nr.nid) AS count FROM {node} np LEFT JOIN {project_release_nodes} prn ON np.nid = prn.pid LEFT JOIN {node} nr ON prn.nid = nr.nid AND nr.created > %d AND nr.created < %d$where GROUP BY np.nid", $args);
     while ($node = db_fetch_object($nodes)) {
       $this->currentSample->values[$node->nid]['releases'] = $node->count;
     }
+  }
+
+  public function trackObjectIDs() {
+    $nids = array();
+    $releases = db_query("SELECT nid FROM {project_projects} WHERE sandbox = 0");
+    while ($release = db_fetch_object($releases)) {
+      $nids[] = $release->nid;
+    }
+    return $nids;
   }
 }
 
