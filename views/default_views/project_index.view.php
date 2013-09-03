d5482937aeb484af83e14378b077f89c313aca14
Neil Drumm <drumm@delocalizedham.com>
2013-09-03 20:34:48 +0000

    [#1807726] Port project index view to D7

--- views/default_views/project_index.view.php
+++ views/default_views/project_index.view.php
@@ -4,7 +4,7 @@
  * @file
  * An index of all projects of a certain project type by title.
  */
-$view = new view;
+$view = new view();
 $view->name = 'project_index';
 $view->description = 'Listing of all projects by title';
 $view->tag = '';
@@ -16,6 +16,7 @@ $view->disabled = FALSE; /* Edit this to true to make a default view disabled in
 
 /* Display: Defaults */
 $handler = $view->new_display('default', 'Defaults', 'default');
+$handler->display->display_options['use_more_always'] = FALSE;
 $handler->display->display_options['access']['type'] = 'none';
 $handler->display->display_options['cache']['type'] = 'none';
 $handler->display->display_options['query']['type'] = 'views_query';
@@ -32,16 +33,6 @@ $handler->display->display_options['fields']['title']['id'] = 'title';
 $handler->display->display_options['fields']['title']['table'] = 'node';
 $handler->display->display_options['fields']['title']['field'] = 'title';
 $handler->display->display_options['fields']['title']['label'] = '';
-$handler->display->display_options['fields']['title']['alter']['alter_text'] = 0;
-$handler->display->display_options['fields']['title']['alter']['make_link'] = 0;
-$handler->display->display_options['fields']['title']['alter']['word_boundary'] = 1;
-$handler->display->display_options['fields']['title']['alter']['ellipsis'] = 1;
-$handler->display->display_options['fields']['title']['alter']['strip_tags'] = 0;
-$handler->display->display_options['fields']['title']['alter']['trim'] = 0;
-$handler->display->display_options['fields']['title']['alter']['html'] = 0;
-$handler->display->display_options['fields']['title']['hide_empty'] = 0;
-$handler->display->display_options['fields']['title']['empty_zero'] = 0;
-$handler->display->display_options['fields']['title']['link_to_node'] = 1;
 /* Sort criterion: Content: Title */
 $handler->display->display_options['sorts']['title']['id'] = 'title';
 $handler->display->display_options['sorts']['title']['table'] = 'node';
@@ -52,14 +43,13 @@ $handler->display->display_options['arguments']['type']['table'] = 'node';
 $handler->display->display_options['arguments']['type']['field'] = 'type';
 $handler->display->display_options['arguments']['type']['ui_name'] = 'Project node type';
 $handler->display->display_options['arguments']['type']['default_action'] = 'not found';
-$handler->display->display_options['arguments']['type']['title_enable'] = 1;
+$handler->display->display_options['arguments']['type']['title_enable'] = TRUE;
 $handler->display->display_options['arguments']['type']['title'] = '%1 index';
 $handler->display->display_options['arguments']['type']['default_argument_type'] = 'fixed';
-$handler->display->display_options['arguments']['type']['default_argument_skip_url'] = 0;
 $handler->display->display_options['arguments']['type']['summary']['number_of_records'] = '0';
 $handler->display->display_options['arguments']['type']['summary']['format'] = 'default_summary';
 $handler->display->display_options['arguments']['type']['summary_options']['items_per_page'] = '25';
-$handler->display->display_options['arguments']['type']['specify_validation'] = 1;
+$handler->display->display_options['arguments']['type']['specify_validation'] = TRUE;
 $handler->display->display_options['arguments']['type']['validate']['type'] = 'project_node_type';
 /* Filter criterion: Content: Published */
 $handler->display->display_options['filters']['status']['id'] = 'status';
@@ -74,14 +64,13 @@ $handler->display->display_options['filters']['field_project_type_value']['table
 $handler->display->display_options['filters']['field_project_type_value']['field'] = 'field_project_type_value';
 $handler->display->display_options['filters']['field_project_type_value']['ui_name'] = 'Project type';
 $handler->display->display_options['filters']['field_project_type_value']['value'] = array(
-  'full' => 'full',
+    'full' => 'full',
 );
 $handler->display->display_options['filters']['field_project_type_value']['exposed'] = TRUE;
 $handler->display->display_options['filters']['field_project_type_value']['expose']['operator_id'] = 'field_project_type_value_op';
 $handler->display->display_options['filters']['field_project_type_value']['expose']['label'] = 'Project type';
 $handler->display->display_options['filters']['field_project_type_value']['expose']['operator'] = 'field_project_type_value_op';
 $handler->display->display_options['filters']['field_project_type_value']['expose']['identifier'] = 'project-status';
-$handler->display->display_options['filters']['field_project_type_value']['expose']['reduce'] = 0;
 
 /* Display: Page */
 $handler = $view->new_display('page', 'Page', 'page_1');
@@ -91,8 +80,9 @@ $handler->display->display_options['path'] = 'project/%/index';
 $handler = $view->new_display('block', 'Block', 'block_1');
 $handler->display->display_options['defaults']['use_more'] = FALSE;
 $handler->display->display_options['use_more'] = TRUE;
-$handler->display->display_options['use_more_always'] = TRUE;
-$handler->display->display_options['use_more_text'] = 'View full index';
+$handler->display->display_options['defaults']['pager'] = FALSE;
+$handler->display->display_options['pager']['type'] = 'some';
+$handler->display->display_options['pager']['options']['items_per_page'] = 4;
 $handler->display->display_options['defaults']['style_plugin'] = FALSE;
 $handler->display->display_options['style_plugin'] = 'list';
 $handler->display->display_options['style_options']['grouping'] = '';
@@ -107,12 +97,11 @@ $handler->display->display_options['arguments']['type']['table'] = 'node';
 $handler->display->display_options['arguments']['type']['field'] = 'type';
 $handler->display->display_options['arguments']['type']['ui_name'] = 'Project Node Type';
 $handler->display->display_options['arguments']['type']['default_action'] = 'default';
-$handler->display->display_options['arguments']['type']['title_enable'] = 1;
+$handler->display->display_options['arguments']['type']['title_enable'] = TRUE;
 $handler->display->display_options['arguments']['type']['title'] = '%1 index';
 $handler->display->display_options['arguments']['type']['default_argument_type'] = 'project_plugin_argument_default_project_type';
-$handler->display->display_options['arguments']['type']['default_argument_skip_url'] = 0;
 $handler->display->display_options['arguments']['type']['summary']['number_of_records'] = '0';
 $handler->display->display_options['arguments']['type']['summary']['format'] = 'default_summary';
 $handler->display->display_options['arguments']['type']['summary_options']['items_per_page'] = '25';
-$handler->display->display_options['arguments']['type']['specify_validation'] = 1;
+$handler->display->display_options['arguments']['type']['specify_validation'] = TRUE;
 $handler->display->display_options['arguments']['type']['validate']['type'] = 'project_node_type';
